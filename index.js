89b48b8abc2cf8136feff3ecd0d379268d87ddd6
And123456789 <47659027+And123456789@users.noreply.github.com>
2021-05-21 19:57:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import{getWeather} from 'fetchWeather.js';
 /*addListeners();
 
 function addListeners(){
@@ -14,14 +15,3 @@ function insertParagraph(text){
 	document.querySelector('body').appendChild(p);
 }
 */
-
-alert("export");
-export function(getWeather){
-	fetch("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.1489/lat/57.3081/data.json")
-	.then((response) => {
-   		return response.json();
-	})
-	.then((data) => {
-		return data;
-	});
-}
