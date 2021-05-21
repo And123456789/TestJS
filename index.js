a16c093f76f50a004008e346cd06b405c386cf03
And123456789 <47659027+And123456789@users.noreply.github.com>
2021-05-21 18:36:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert("index");
+addListeners();
+
+function addListeners(){
+	document.querySelector('button').addEventListener('click', quoteButtonClicked);
+}
+
+function quoteButtonClicked(){
+	insertParagraph('Life')
+}
+
+function insertParagraph(text){
+	const p = document.createElement('p');
+		p.innerText = text;
+	document.querySelector('body').appendChild(p);
+}
