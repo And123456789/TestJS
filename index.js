e521617a5760c4c43fa33d5d5f78502d54eb4da8
And123456789 <47659027+And123456789@users.noreply.github.com>
2021-05-21 19:53:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,15 +15,13 @@ function insertParagraph(text){
 }
 */
 
-alert("hejsan");
-
-function getData(){
-    return fetch("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.1489/lat/57.3081/data.json")
-.then((response) => {
-    return response.json();
-})
-.then((data) => {
-    return data;
-});
+alert("export");
+export function(getWeather){
+	fetch("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.1489/lat/57.3081/data.json")
+	.then((response) => {
+   		return response.json();
+	})
+	.then((data) => {
+		return data;
+	});
 }
-alert(data);
