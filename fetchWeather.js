98cce4ef2b9b655c6cf7b2ad5bd562b215f7511d
And123456789 <47659027+And123456789@users.noreply.github.com>
2021-05-21 20:14:22 +0200

    Update fetchWeather.js

--- fetchWeather.js
+++ fetchWeather.js
@@ -1,6 +1,6 @@
 alert("export");
 export function(getWeather){
-	fetch("https://quotes.rest/qod') //opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.1489/lat/57.3081/data.json")
+	return fetch("https://quotes.rest/qod') //opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.1489/lat/57.3081/data.json")
 	.then((response) => {
    		return response.json();
 	})
